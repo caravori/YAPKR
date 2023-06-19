28c8768d7b207d9bbe05725977be7d5b51968875
caravori <gabrielscavazini@gmail.com>
2023-06-19 14:03:34 -0300

    commit n lembro

--- YAPKR/view/Pokemon.js
+++ YAPKR/view/Pokemon.js
@@ -1,20 +1,35 @@
-import {Image, Text, View} from "react-native";
+import {Image, View} from "react-native";
 import {useContext} from "react";
 import {Context} from "../components/Context";
 import {Avatar} from "@rneui/themed";
 import {getColor} from "./Pokedex";
 
 
-const Pokemon = ({route}) =>{
+function GetAsset(type) {
+    return (
+        <Image source={{uri: '../assets/' + type.toString() + '.png'}}/>
+    )
+}
+
+const Pokemon = ({route}) => {
+
     const {state} = useContext(Context);
     const {pokemon} = route.params.pokemon;
     let color = getColor(pokemon.types[0].type.name)
+
     console.log(pokemon.sprites.other['official-artwork'].front_default)
-    return(
-        <View style={{flex:1}}>
-            <View style={{alignItems:'center', backgroundColor:color[0], borderRadius:50, marginTop:20}}>
-                <Avatar source={{uri: pokemon.sprites.other['official-artwork'].front_default}} size={200}/>
+    return (
+        <View style={{flex: 1}}>
+            <View style={{alignItems: 'center', borderRadius: 50, marginTop: 20}}>
+                <!--
+                   TODO: Para cada tipo, um background diferente tem alguns nos assets. 10 no total. Temos 16 tipos. Faltam 6 backgrounds
+                -->
+                <GetAsset/>
+                <View style={{alignItems:'center'}}>
+                    <Avatar source={{uri: pokemon.sprites.other['official-artwork'].front_default}} size={200}/>
+                </View>
             </View>
+
         </View>
     )
 }
